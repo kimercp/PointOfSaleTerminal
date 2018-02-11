bfb6a12b11d1b788c0fc27eb0f910927828386fa
WyrwiGnatus <30984606+kimercp@users.noreply.github.com>
2018-02-11 15:41:29 +0000

    Finished SignInActivity layout

---
+++ app/src/main/java/com/kimersoft/pointofsaleterminal/SignInActivity.java
@@ -0,0 +1,139 @@
+package com.kimersoft.pointofsaleterminal;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.kimersoft.pointofsaleterminal.utils.LottieAnimations;
+import com.kimersoft.pointofsaleterminal.utils.SplashScreen;
+import com.kimersoft.pointofsaleterminal.volleyHelpers.VolleyRequestsHandler;
+import com.kimersoft.pointofsaleterminal.volleyHelpers.VolleyCallback;
+import com.kimersoft.pointofsaleterminal.models.User;
+import com.kimersoft.pointofsaleterminal.volleyHelpers.ObjectToJsonParser;
+import com.airbnb.lottie.LottieAnimationView;
+import org.json.JSONObject;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private EditText etSignInEmail, etSignInPwd;
+    private Button btnSignIn;
+
+    private RelativeLayout rlLoading;
+    private LottieAnimationView loading;
+    private LottieAnimations lottieAnimations;
+
+    // my added variables
+    private VolleyRequestsHandler volleyRequestsHandler;
+    private ObjectToJsonParser usersDetails;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        findViewById(R.id.rl_sign_in).setBackground(ContextCompat.getDrawable(this,R.drawable.gradient_background_radial));
+
+        // full screen on device
+        makeFullscreen();
+        // make a nice animation effect from not visible to visible screen (splash screen)
+        new SplashScreen(this, 4000);
+
+
+
+        //Initializing values
+        lottieAnimations = new LottieAnimations();
+
+        //Bind UI
+        etSignInEmail = findViewById(R.id.et_sign_in_email);
+        etSignInPwd = findViewById(R.id.et_sign_in_pwd);
+        btnSignIn = findViewById(R.id.btn_sign_in);
+        rlLoading = findViewById(R.id.rl_sign_in_loading);
+        loading = findViewById(R.id.loading);
+
+        //Sign in action
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email, password;
+                email = etSignInEmail.getText().toString();
+                password = etSignInPwd.getText().toString();
+
+                //Starting Loading animation
+                lottieAnimations.StartSignInLoading(loading, rlLoading);
+
+                // create handler
+                volleyRequestsHandler = new VolleyRequestsHandler(SignInActivity.this);
+                // create object with user's email and password
+                usersDetails = new ObjectToJsonParser();
+                JSONObject userDetailsJSONObject = usersDetails.userSignInJson(email, password);
+
+                volleyRequestsHandler.userSignIn(userDetailsJSONObject,
+                        new VolleyCallback() {
+                            @Override
+                            public void onSuccess(Object response) {
+                                /*
+                                * the login end point return Token
+                                * we save the email, password and the returned token to shareprefs then we call
+                                * user_info endpoint to retrieve wallet,email,name,lastname and type parameters
+                                */
+                                final String token = (String) response;
+                                User u = new User();
+                                u.setToken(token);
+                                u.setEmail(email);
+                                u.setPassword(password);
+                                User.setCurrentUserAfterLogin(u, SignInActivity.this);
+                                //user_info endpoint
+                                new VolleyRequestsHandler(SignInActivity.this).getUserInfo(token, new VolleyCallback() {
+                                    @Override
+                                    public void onSuccess(Object respons) {
+
+                                        lottieAnimations.StoppingSignInLoading(loading, rlLoading);
+                                        startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                                    }
+
+                                    @Override
+                                    public void onError(Object error) {
+                                        lottieAnimations.StoppingSignInLoading(loading, rlLoading);
+                                    }
+                                });
+
+                            }
+
+                            @Override
+                            public void onError(Object error) {
+
+                                Toast.makeText(SignInActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                                lottieAnimations.StoppingSignInLoading(loading, rlLoading);
+                            }
+                        });
+
+            }
+        });
+
+    }
+
+    /* Hide UI action bar and make the app fullscreen */
+    public final void makeFullscreen() {
+        getSupportActionBar().hide();
+        // API 19 (Kit Kat)
+        if (Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(
+//                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        } else {
+            if (Build.VERSION.SDK_INT > 10) {
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
+        }
+    }
+}
